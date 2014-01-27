5c1e89f52f118198d258a6347a17e47d270fd1f3
Fear837 <fear837@web.de>
2014-01-27 17:12:50 +0100

    *EntityListener: Ein paar Bugs gefixt und Potions werden geblockt.
    Player, Egg, Snowball, Arrow, ThrownPotion werden jetzt blockiert.
    Außerdem ist der Code jetzt ein wenig aufgeraeumter.

--- src/de/Fear837/EntityListener.java
+++ src/de/Fear837/EntityListener.java
@@ -1,23 +1,21 @@
 package de.Fear837;
 
 import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Egg;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
+import org.bukkit.entity.ThrownPotion;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.potion.Potion;
 
 public final class EntityListener implements Listener {
 
 	private Main plugin;
 	private MySQL sql;
-
-	/*
-	 * // Still unused variables private DamageCause entityAttack =
-	 * DamageCause.ENTITY_ATTACK; private DamageCause entityProjectile =
-	 * DamageCause.PROJECTILE;
-	 */
 
 	/* Der Entity-Listener */
 	public EntityListener(MySQL sql, Main plugin) {
@@ -27,10 +25,9 @@ public final class EntityListener implements Listener {
 
 	@EventHandler
 	public void onEntityDamage(EntityDamageByEntityEvent event) {
-		if (sql == null && event.isCancelled()) {
-			return;
-		}
+		if (sql == null || event.isCancelled()) { return; }
 		EntityType entityType = event.getEntityType();
+		
 		if (entityType == EntityType.COW 
 				|| entityType == EntityType.PIG
 				|| entityType == EntityType.SHEEP
@@ -40,51 +37,88 @@ public final class EntityListener implements Listener {
 
 			Entity entity = event.getEntity();
 			Entity damager = event.getDamager();
+			String entityOwner = null;
+			
+			try { entityOwner = Commands.getEntityOwner(entity.getUniqueId()); }
+			catch (Exception e) { e.printStackTrace(); }
+			if (entityOwner == null || entityOwner.isEmpty())
+			{ return; }
 
-			plugin.getServer().broadcastMessage("DEBUG::EntityType: "+damager.getType());
+			if (plugin.getConfig().getBoolean("settings.debug-messages")) {
+				plugin.getServer().broadcastMessage("DEBUG::EntityType: "+damager.getType() + " | Owner:" + entityOwner);
+			}
 			
 			switch (event.getDamager().getType()) {
 			case PLAYER:
-				String entityOwner = null;
-				try {
-					entityOwner = Commands.getEntityOwner(entity.getUniqueId());
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
 
-				plugin.getServer().broadcastMessage("Entity-Damage-Event::EntityOwner: " + entityOwner);
-
-				if ((entityOwner != null && !entityOwner.isEmpty() && !((Player) damager)
-						.getName().equalsIgnoreCase(entityOwner))) {
-					((Player) damager).sendMessage("Das Tier ist von "
-							+ entityOwner + " gesichert!");
-					event.setCancelled(true);
-					return;
+				if (!((Player) damager).getName().equalsIgnoreCase(entityOwner))
+				{
+					((Player) damager).sendMessage("Das Tier ist von " + entityOwner + " gesichert!");
+					event.setCancelled(true); return;
 				}
 				break;
 			case ARROW:
 				plugin.getServer().broadcastMessage("Arrow detected...");
-				String entityOwner1 = null;
-				Arrow projectile = (Arrow)entity;
-				try {
-					entityOwner1 = Commands.getEntityOwner(projectile.getShooter().getUniqueId());
-				} catch (Exception e) { e.printStackTrace(); }
+			    Arrow projectile = (Arrow)damager;
+			    Entity shooter = null;
+			    try { shooter = projectile.getShooter(); }
+			    catch (Exception e) { return; }
 				
-				if ((entityOwner1 != null && !entityOwner1.isEmpty())) {
-					if (!((Player) projectile.getShooter()).getName().equalsIgnoreCase(entityOwner1)) {
-						event.setCancelled(true);
-						
-						return;
+				if (shooter != null) {
+					if (shooter instanceof Player) {
+						Player player = (Player)shooter;
+						if (!player.getName().equalsIgnoreCase(entityOwner))
+						{ event.setCancelled(true); return; }
 					}
 				}
-				
 				break;
 			case EGG:
 				plugin.getServer().broadcastMessage("Egg detected...");
+				Egg projectile2 = (Egg)damager;
+			    Entity shooter2 = null;
+			    try { shooter2 = projectile2.getShooter(); }
+			    catch (Exception e) { return; }
+				
+				if (shooter2 != null) {
+					if (shooter2 instanceof Player) {
+						Player player = (Player)shooter2;
+						if (!player.getName().equalsIgnoreCase(entityOwner))
+						{ event.setCancelled(true); return; }
+					}
+				}
+				break;
+			case SNOWBALL:
+				plugin.getServer().broadcastMessage("Snowball detected...");
+				Snowball projectile3 = (Snowball)damager;
+			    Entity shooter3 = null;
+			    try { shooter3 = projectile3.getShooter(); }
+			    catch (Exception e) { return; }
+				
+				if (shooter3 != null) {
+					if (shooter3 instanceof Player) {
+						Player player = (Player)shooter3;
+						if (!player.getName().equalsIgnoreCase(entityOwner))
+						{ event.setCancelled(true); return; }
+					}
+				}
+				break;
+			case SPLASH_POTION:
+				plugin.getServer().broadcastMessage("Potion detected...");
+				ThrownPotion projectile4 = (ThrownPotion)damager;
+			    Entity shooter4 = null;
+			    try { shooter4 = projectile4.getShooter(); }
+			    catch (Exception e) { return; }
+				
+				if (shooter4 != null) {
+					if (shooter4 instanceof Player) {
+						Player player = (Player)shooter4;
+						if (!player.getName().equalsIgnoreCase(entityOwner))
+						{ event.setCancelled(true); return; }
+					}
+				}
 				break;
 			default:
-				plugin.getServer().broadcastMessage(
-						"Damager detected: " + event.getDamager().getType());
+				plugin.getServer().broadcastMessage("Unknown Damager detected: " + event.getDamager().getType());
 				break;
 			}
 		}
