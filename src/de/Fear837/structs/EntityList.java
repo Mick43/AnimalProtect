745ee02fc7d00995ece98312940790a3f3c6c73e
Fear837 <fear837@web.de>
2014-01-31 19:58:16 +0100

    +EntityList.java: Klasse erstellt.

---
+++ src/de/Fear837/structs/EntityList.java
@@ -0,0 +1,13 @@
+package de.Fear837.structs;
+
+import de.Fear837.Main;
+import de.Fear837.MySQL;
+
+public class EntityList {
+	
+	/** The plugin using this list */
+	private Main plugin;
+	/** The database to get data from */
+	private MySQL database;
+
+}
