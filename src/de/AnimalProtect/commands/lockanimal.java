cb0165b4687122cce8b1ff85c4d85a24d7224b79
Fear837 <fear837@web.de>
2014-02-02 17:12:24 +0100

    +/lockanimal: Befehl geschrieben.

--- src/de/AnimalProtect/commands/lockanimal.java
+++ src/de/AnimalProtect/commands/lockanimal.java
@@ -3,9 +3,13 @@ package de.AnimalProtect.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 
 import de.AnimalProtect.Main;
 import de.AnimalProtect.MySQL;
+import de.AnimalProtect.listener.InteractEventListener;
 import de.AnimalProtect.structs.EntityList;
 
 public class lockanimal implements CommandExecutor {
@@ -22,10 +26,46 @@ public class lockanimal implements CommandExecutor {
 	
 	@Override
 	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
-		if (plugin.isEnabled() && database.checkConnection()) {
-			// TODO: Command -> /lockanimal
+		if (plugin.isEnabled() && database.checkConnection() && cs instanceof Player) {
+			/* Variablen initialisieren */
+			Player player = (Player)cs;
+			Entity entity = InteractEventListener.getSelected(player);
+			
+			/* Prüfen ob ein Tier ausgewählt wurde. */
+			if (entity == null) { 
+				player.sendMessage("§cFehler: Es wurde kein Tier ausgewählt!");
+				return true;
+			}
+			
+			/* Prüfen ob das Tier bereits gelockt wurde */
+			if (list.containsEntity(entity)) { 
+				player.sendMessage("§cFehler: Das Tier ist bereits protected!");
+				return true;
+			}
+			
+			/* Das Tier wird gelockt. */
+			list.lock(player.getName(), entity);
+			
+			/* Prüfen ob das locken geklappt hat */
+			if (!list.lastActionSucceeded()) {
+				player.sendMessage("§cFehler: Das Tier konnte nicht gelockt werden!");
+				return true;
+			}
+			
+			player.sendMessage("§aDas Tier wurde erfolgreich gesichert!");
 		}
 		return false;
 	}
-
+	
+	private boolean isAnimal(Entity entity) {
+		EntityType type = entity.getType();
+		if (type == EntityType.SHEEP
+		||  type == EntityType.PIG
+		||  type == EntityType.COW
+		||  type == EntityType.CHICKEN
+		||  type == EntityType.HORSE
+		||  type == EntityType.WOLF)
+		{ return true; }
+		return false;
+	}
 }
