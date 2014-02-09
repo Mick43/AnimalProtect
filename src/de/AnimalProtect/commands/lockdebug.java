8ea524f2921dcc7d7ed3a19b48ff3d63602a7207
Fear837 <fear837@web.de>
2014-02-09 15:10:42 +0100

    +lockdebug: Added playSound
    Man kann über den lockdebug-Befehl jetzt bestimmte Sounds abspielen. Das
    dient nur zum Testen

--- src/de/AnimalProtect/commands/lockdebug.java
+++ src/de/AnimalProtect/commands/lockdebug.java
@@ -1,8 +1,10 @@
 package de.AnimalProtect.commands;
 
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import de.AnimalProtect.Main;
 import de.AnimalProtect.MySQL;
@@ -30,6 +32,14 @@ public class lockdebug implements CommandExecutor {
 		}
 		else {
 			cs.sendMessage("§cFehler: Es besteht keine Verbindung zur Datenbank!");
+		}
+		
+		if (args.length == 3 && cs instanceof Player) {
+			Player player = (Player)cs;
+			try {
+				player.playSound(player.getLocation(), Sound.valueOf(args[0]), Float.parseFloat(args[1]), Float.parseFloat(args[2]));
+			}
+			catch (Exception e) { }
 		}
 		
 		return true;
