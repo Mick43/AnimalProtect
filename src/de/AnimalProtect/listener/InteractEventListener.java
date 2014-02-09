f94bbd3c169285640519a713b8cde63c175fee0b
Fear837 <fear837@web.de>
2014-02-09 15:44:39 +0100

    +InteractEventListener: Added new EntityTypes
    Man kann jetzt auch Iron_Golems, Schneemänner und Katzen protecten.

--- src/de/AnimalProtect/listener/InteractEventListener.java
+++ src/de/AnimalProtect/listener/InteractEventListener.java
@@ -99,7 +99,10 @@ public class InteractEventListener implements Listener {
 		||  type == EntityType.COW
 		||  type == EntityType.CHICKEN
 		||  type == EntityType.HORSE
-		||  type == EntityType.WOLF)
+		||  type == EntityType.WOLF
+		||  type == EntityType.IRON_GOLEM
+		||  type == EntityType.SNOWMAN
+		||  type == EntityType.OCELOT)
 		{ return true; }
 		return false;
 	}
