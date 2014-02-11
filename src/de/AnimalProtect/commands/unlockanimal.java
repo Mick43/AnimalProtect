4dbf92b92336fe6969ab5f0a2e2cd51414b84aa3
Fear837 <fear837@web.de>
2014-02-11 19:21:53 +0100

    +unlockanimal: Den Befehl geschrieben.
    Der Befehl /unlockanimal funktioniert jetzt.

--- src/de/AnimalProtect/commands/unlockanimal.java
+++ src/de/AnimalProtect/commands/unlockanimal.java
@@ -1,12 +1,15 @@
 package de.AnimalProtect.commands;
 
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 import de.AnimalProtect.Main;
 import de.AnimalProtect.MySQL;
+import de.AnimalProtect.listener.InteractEventListener;
 import de.AnimalProtect.structs.EntityList;
 
 public class unlockanimal implements CommandExecutor {
@@ -24,7 +27,34 @@ public class unlockanimal implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
 		if (plugin.isEnabled() && cs instanceof Player) {
-			// TODO: Command -> /unlockanimal 
+			/* Variablen initialisieren */
+			Player player = (Player)cs;
+			Entity entity = InteractEventListener.getSelected(player);
+			
+			/* Prüfen ob ein Tier ausgewählt wurde. */
+			if (entity == null) { 
+				player.sendMessage("§cFehler: Es wurde kein Tier ausgewählt!");
+				return true;
+			}
+			
+			/* Prüfen ob das Tier bereits gelockt wurde */
+			if (!list.containsEntity(entity)) { 
+				player.sendMessage("§cFehler: Das Tier ist noch nicht protected!");
+				return true;
+			}
+			
+			/* Das Tier wird gelockt. */
+			list.unlock(entity.getUniqueId());
+			
+			/* Prüfen ob das locken geklappt hat */
+			if (!list.lastActionSucceeded()) {
+				player.sendMessage("§cFehler: Die Protection konnte nicht enfernt werden!");
+				return true;
+			}
+			
+			player.playSound(player.getLocation(), Sound.CLICK, 2.0f, 2.5f);
+			player.sendMessage("§aDie Protection wurde erfolgreich entfernt!");
+			return true;
 		}
 		
 		return true;
