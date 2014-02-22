9e5b4625d4e0fa9de075ae62972bb7db8e432526
Fear837 <fear837@web.de>
2014-02-22 22:46:31 +0100

    *DeathEventListener: Bugfix
    Das getötete Tier wird nun unlocked, wenn der Mörder der Owner ist.

--- src/de/AnimalProtect/listener/DeathEventListener.java
+++ src/de/AnimalProtect/listener/DeathEventListener.java
@@ -2,13 +2,16 @@ package de.AnimalProtect.listener;
 
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 
 import de.AnimalProtect.Main;
 import de.AnimalProtect.MySQL;
 import de.AnimalProtect.structs.EntityList;
+import de.AnimalProtect.utility.APLogger;
 
 public class DeathEventListener implements Listener {
 	
@@ -24,11 +27,39 @@ public class DeathEventListener implements Listener {
 	
 	@EventHandler
 	public void onEntityDeath(EntityDeathEvent event) {
+		/* Datenbank-Verbindung aufbauen, falls nicht vorhanden. */
+		if (!database.checkConnection()) { database.openConnection(); }
+		
 		if (plugin.isEnabled() && database.checkConnection() && isAnimal(event.getEntity())) {
+			/* Variablen bereitstellen */
+			EntityDamageByEntityEvent e = null;
+			String deathCause = event.getEntity().getLastDamageCause().getCause().toString();
+			Entity entity = event.getEntity();
+			Player damager = null;
 			
-			String deathCause = event.getEntity().getLastDamageCause().toString();
+			/* Prüfen ob das Tier ermordet wurde */
+			try { e = (EntityDamageByEntityEvent) event.getEntity().getLastDamageCause(); }
+			catch (Exception exception) { }
+			
+			/* Prüfen ob der Mörder ein Spieler ist */
+			if (e != null) {
+				if (e.getDamager() instanceof Player) {
+					damager = (Player) e.getDamager();
+					
+					/* Prüfen ob der Mörder der Owner ist */
+					if (list.getPlayer(entity.getUniqueId()).equalsIgnoreCase(damager.getName())) {
+						list.unlock(entity.getUniqueId());
+						return;
+					}
+					else {
+						APLogger.warn("Warnung: Ein gelocktes Entity wurde gerade von einem Spieler getötet, der nicht der Owner ist.");
+						APLogger.warn("Eventuell wird irgendein Damage-Event nicht abgefangen!");
+					}
+				}
+			}
+			
+			/* Der Mörder ist kein Spieler, also updaten */
 			database.write("UPDATE ap_entities SET deathcause='"+deathCause+"' WHERE uuid='"+event.getEntity().getUniqueId()+"';", true);
-			
 			list.updateEntity(event.getEntity(), false);
 		}
 	}
