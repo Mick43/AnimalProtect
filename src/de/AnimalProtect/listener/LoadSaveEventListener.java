d137fea7bc0b7266d17b7d34d06b2efa93741f9a
Fear837 <fear837@web.de>
2014-03-29 20:52:12 +0100

    -LoadSaveListener: Removed unused variables

--- src/de/AnimalProtect/listener/LoadSaveEventListener.java
+++ src/de/AnimalProtect/listener/LoadSaveEventListener.java
@@ -6,19 +6,13 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 import de.AnimalProtect.Main;
 import de.AnimalProtect.MySQL;
-import de.AnimalProtect.structs.EntityList;
-import de.AnimalProtect.utility.APLogger;
 
 public class LoadSaveEventListener implements Listener {
 	
-	private Main plugin;
 	private MySQL database;
-	private EntityList list;
 	
 	public LoadSaveEventListener(Main plugin) {
-		this.plugin = plugin;
 		this.database = plugin.database;
-		this.list = plugin.list;
 	}
 	
 
