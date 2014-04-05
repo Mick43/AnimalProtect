de473b88554e1640837a9c82487d3691a1288a13
Fear837 <fear837@web.de>
2014-04-05 18:24:44 +0200

    +AnimalProtect: onEnable() angefangen

--- src/de/AnimalProtect/AnimalProtect.java
+++ src/de/AnimalProtect/AnimalProtect.java
@@ -4,13 +4,45 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class AnimalProtect extends JavaPlugin {
 	
+	private Database database;
+	private boolean debugmode;
+	
 	@Override
 	public void onEnable() {
+		/* Konsole benachrichtigen */
+		Messenger.log("Initializing AnimalProtect...");
 		
+		/* Die Config laden */
+		initializeConfig();
+		
+		/* Die Datenbank laden */
+		initializeDatabase();
 	}
 	
 	@Override
 	public void onDisable() {
 		
+	}
+	
+	private void initializeConfig() {
+		Messenger.log("  Loading Config...");
+		
+		try {
+			getConfig().options().copyDefaults(true);
+			saveConfig();
+		}
+		catch (Exception e) { 
+			Messenger.warn("Failed to load the config file!");
+		}
+	}
+	
+	private void initializeDatabase() {
+		Messenger.log(" Loading Database...");
+		
+		this.database = new Database(this);
+	}
+	
+	public boolean isDebugging() {
+		return debugmode;
 	}
 }
