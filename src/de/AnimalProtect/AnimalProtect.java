240c462cc327af5e96b3bae30d94067950fe7b94
Fear837 <fear837@web.de>
2014-04-05 21:34:50 +0200

    +AnimalProtect: Added isAnimal

--- src/de/AnimalProtect/AnimalProtect.java
+++ src/de/AnimalProtect/AnimalProtect.java
@@ -1,6 +1,12 @@
 package de.AnimalProtect;
 
+import java.util.UUID;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import de.AnimalProtect.listeners.InteractEventListener;
 
 public class AnimalProtect extends JavaPlugin {
 	
@@ -44,6 +50,26 @@ public class AnimalProtect extends JavaPlugin {
 	
 	public boolean isDebugging() {
 		return debugmode;
+	}
+	
+	public boolean isAnimal(Entity entity) {
+		EntityType type = entity.getType();
+		if (type == EntityType.SHEEP
+		||  type == EntityType.PIG
+		||  type == EntityType.COW
+		||  type == EntityType.CHICKEN
+		||  type == EntityType.HORSE
+		||  type == EntityType.WOLF
+		||  type == EntityType.IRON_GOLEM
+		||  type == EntityType.SNOWMAN
+		||  type == EntityType.VILLAGER
+		||  type == EntityType.OCELOT)
+		{ return true; }
+		return false;
+	}
+	
+	public Entity getSelectedAnimal(UUID uuid) {
+		return InteractEventListener.getSelected(uuid);
 	}
 	
 	public Database getDatenbank() {
