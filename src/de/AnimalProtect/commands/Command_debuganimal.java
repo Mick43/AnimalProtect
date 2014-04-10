610a092c6e11451839fa36f30854919a240e3c91
Fear837 <fear837@web.de>
2014-04-10 16:35:11 +0200

    +AnimalProtect.Commands: Command-Klassen erstellt!

---
+++ src/de/AnimalProtect/commands/Command_debuganimal.java
@@ -0,0 +1,23 @@
+package de.AnimalProtect.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import de.AnimalProtect.AnimalProtect;
+
+public class Command_debuganimal implements CommandExecutor {
+	
+	private AnimalProtect plugin;
+	
+	public Command_debuganimal(AnimalProtect plugin) {
+		this.plugin = plugin;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
+		if (!plugin.isEnabled()) { return false; }
+		//TODO: Command
+		return true;
+	}
+}
---
+++ src/de/AnimalProtect/commands/Command_lockanimal.java
@@ -0,0 +1,67 @@
+package de.AnimalProtect.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+import craftoplugin.core.CraftoMessenger;
+import craftoplugin.core.database.CraftoPlayer;
+import de.AnimalProtect.AnimalProtect;
+import de.AnimalProtect.structs.Animal;
+import de.AnimalProtect.structs.AnimalType;
+
+public class Command_lockanimal implements CommandExecutor {
+	
+	private AnimalProtect plugin;
+	
+	public Command_lockanimal(AnimalProtect plugin) {
+		this.plugin = plugin;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
+		if (!plugin.isEnabled()) { return false; }
+		if (cmd.getName().equalsIgnoreCase("lockanimal")) { Command_lockanimal.runCommand(cs, args); }
+		
+		return true;
+	}
+	
+	public static void runCommand(CommandSender cs, String[] args) {
+		if (!(cs instanceof Player)) {
+			CraftoMessenger.message(cs, "§cFehler: Dieser Befehl kann nur von einem Spieler ausgeführt werden!");
+			return;
+		}
+		
+		Player sender = (Player)cs;
+		CraftoPlayer player = CraftoPlayer.getPlayer(sender.getUniqueId());
+		Entity entity = AnimalProtect.plugin.getSelectedAnimal(sender.getUniqueId());
+		
+		if (entity == null) {
+			CraftoMessenger.message(cs, "§cFehler: Du hast zurzeit noch kein Tier ausgewählt!");
+			return;
+		}
+		else if (player == null) {
+			CraftoMessenger.message(cs, "§cFehler: Dein Spielerobjekt wurde nicht gefunden! Bitte kontaktiere einen Administrator.");
+		}
+		
+		Animal animal = AnimalProtect.plugin.getDatenbank().getAnimal(entity.getUniqueId().toString());
+		
+		if (animal != null) {
+			CraftoMessenger.message(cs, "§cFehler: Das Tier ist bereits protected!");
+			return;
+		}
+		else {
+			animal = new Animal(AnimalProtect.plugin, player, entity);
+			if(animal.saveToDatabase(true)) {
+				CraftoMessenger.message(cs, "§aDas Tier wurde erfolgreich gesichert!");
+			}
+			else {
+				CraftoMessenger.message(cs, "§cFehler: Das Tier konnte nicht gesichert werden!");
+			}
+		}
+		
+		
+	}
+}
