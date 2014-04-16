933146092ce7b905e05331a26fa3bb4b6e2a7713
Fear837 <fear837@web.de>
2014-04-16 18:07:27 +0200

    +Command_lockedanimals: Kommando geschrieben

--- src/de/AnimalProtect/commands/Command_lockedanimals.java
+++ src/de/AnimalProtect/commands/Command_lockedanimals.java
@@ -1,10 +1,15 @@
 package de.AnimalProtect.commands;
 
+import java.util.UUID;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
+import craftoplugin.core.database.CraftoPlayer;
 import de.AnimalProtect.AnimalProtect;
+import de.AnimalProtect.Messenger;
 
 public class Command_lockedanimals implements CommandExecutor {
 	
@@ -17,7 +22,37 @@ public class Command_lockedanimals implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
 		if (!plugin.isEnabled()) { return false; }
-		//TODO: Command
+		Command_lockedanimals.runCommand(cs, args);
 		return true;
+	}
+	
+	public static void runCommand(CommandSender cs, String[] args) {
+		CraftoPlayer player = null;
+		if (args.length == 0) {
+			if (cs instanceof Player) {
+				player = CraftoPlayer.getPlayer(((Player)cs).getUniqueId());
+			}
+			else { Messenger.sendMessage(cs, "§cFehler: Es wurde kein Spieler angegeben!"); }
+		}
+		else if (args.length == 1) {
+			if (isUUID(args[0])) {
+				player = CraftoPlayer.getPlayer(UUID.fromString(args[0]));
+			}
+			else { player = CraftoPlayer.getPlayer(args[0]); }
+		}
+		else { Messenger.sendMessage(cs, "§cFehler: Zu viele Argumente angegeben!"); }
+		
+		if (player == null) { Messenger.sendMessage(cs, "§cFehler: Der Spieler konnte nicht gefunden werden!"); }
+		
+		Integer count = AnimalProtect.plugin.getDatenbank().getAnimals(player.getUniqueId()).size();
+		if (cs.getName().equalsIgnoreCase(player.getName())) {
+			Messenger.sendMessage(cs, "Du hast insgesamt §6"+count+"§e Tiere gesichert.");
+		}
+		else { Messenger.sendMessage(cs, "Der Spieler §6"+player.getName()+"§e hat insgesamt "+count+" Tiere gesichert."); }
+	}
+	
+	private static boolean isUUID(String value) {
+		//TODO: isUUID
+		return false;
 	}
 }
