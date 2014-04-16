06129dabb9b4979b64068c383071ea97673d894c
Fear837 <fear837@web.de>
2014-04-16 17:10:38 +0200

    +Command_AnimalProtect: /help geschrieben

--- src/de/AnimalProtect/commands/Command_AnimalProtect.java
+++ src/de/AnimalProtect/commands/Command_AnimalProtect.java
@@ -1,5 +1,6 @@
 package de.AnimalProtect.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,7 +18,15 @@ public class Command_AnimalProtect implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
 		if (!plugin.isEnabled()) { return false; }
-		//TODO: Command
+		
+		if (cmd.getName().equalsIgnoreCase("ap") || cmd.getName().equalsIgnoreCase("animalprotect")) {
+			if (args.length < 1) { Command_AnimalProtect.Command_ShowHelp(cs, args); }
+			else if (args[0].equalsIgnoreCase("help")) { Command_AnimalProtect.Command_ShowHelp(cs, args); }
+		}
 		return true;
+	}
+	
+	public static void Command_ShowHelp(CommandSender cs, String[] args) {
+		Bukkit.getServer().dispatchCommand(cs, "help animalprotect");
 	}
 }
