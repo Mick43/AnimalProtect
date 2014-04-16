1206088bba1d0302b533c78a82e0d76d12d4991d
Fear837 <fear837@web.de>
2014-04-16 23:12:09 +0200

    +AnimalProtect: initializeCommands geschrieben

--- src/de/AnimalProtect/AnimalProtect.java
+++ src/de/AnimalProtect/AnimalProtect.java
@@ -1,11 +1,24 @@
 package de.AnimalProtect;
 
+/* Java Imports */
 import java.util.UUID;
 
+/* Bukkit Imports */
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/* AnimalProtect - Command Imports */
+import de.AnimalProtect.commands.Command_AnimalProtect;
+import de.AnimalProtect.commands.Command_debuganimal;
+import de.AnimalProtect.commands.Command_listanimals;
+import de.AnimalProtect.commands.Command_lockanimal;
+import de.AnimalProtect.commands.Command_lockedanimals;
+import de.AnimalProtect.commands.Command_respawnanimal;
+import de.AnimalProtect.commands.Command_tpanimal;
+import de.AnimalProtect.commands.Command_unlockanimal;
+
+/* AnimalProtect - Listener Imports */
 import de.AnimalProtect.listeners.InteractEventListener;
 
 public class AnimalProtect extends JavaPlugin {
@@ -67,14 +80,14 @@ public class AnimalProtect extends JavaPlugin {
 	
 	private void initializeCommands() {
 		try {
-			this.getCommand("ap").setExecutor(null);
-			this.getCommand("lockanimal").setExecutor(null);
-			this.getCommand("unlockanimal").setExecutor(null);
-			this.getCommand("respawnanimal").setExecutor(null);
-			this.getCommand("listanimals").setExecutor(null);
-			this.getCommand("tpanimal").setExecutor(null);
-			this.getCommand("debuganimal").setExecutor(null);
-			this.getCommand("lockedanimals").setExecutor(null);
+			this.getCommand("ap").setExecutor(new Command_AnimalProtect(this));
+			this.getCommand("debuganimal").setExecutor(new Command_debuganimal(this));
+			this.getCommand("listanimals").setExecutor(new Command_listanimals(this));
+			this.getCommand("lockanimal").setExecutor(new Command_lockanimal(this));
+			this.getCommand("lockedanimals").setExecutor(new Command_lockedanimals(this));
+			this.getCommand("respawnanimal").setExecutor(new Command_respawnanimal(this));
+			this.getCommand("tpanimal").setExecutor(new Command_tpanimal(this));
+			this.getCommand("unlockanimal").setExecutor(new Command_unlockanimal(this));
 		}
 		catch (Exception e) {
 			Messenger.exception("AnimalProtect.java/initializeCommands", "Failed to initialize some commands.", e);
