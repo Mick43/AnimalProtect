cd3bf05ad86007c1322a97f4c085dc55345d2b9e
Fear837 <fear837@web.de>
2014-04-17 15:27:42 +0200

    +PlayerEventListener: Klasse erstellt

--- src/de/AnimalProtect/listeners/PlayerEventListener.java
+++ src/de/AnimalProtect/listeners/PlayerEventListener.java
@@ -1,9 +1,15 @@
 package de.AnimalProtect.listeners;
 
+/* Bukkit Imports */
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
+/* AnimalProtect Imports */
 import de.AnimalProtect.AnimalProtect;
 import de.AnimalProtect.Database;
+import de.AnimalProtect.Messenger;
 
 public class PlayerEventListener implements Listener {
 	
@@ -12,6 +18,21 @@ public class PlayerEventListener implements Listener {
 	
 	public PlayerEventListener(AnimalProtect plugin) {
 		this.plugin = plugin;
-		//this.database = AnimalProtect.getDatenbank();
+		this.database = plugin.getDatenbank();
+	}
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		if (!plugin.isEnabled()) { return; }
+		
+		if (isAdmin(event.getPlayer()) && !database.isConnected()) {
+			Messenger.sendMessage(event.getPlayer(), "§c[!] §7Warnung: AnimalProtect konnte keine Verbindung zur Datenbank herstellen!");
+		}
+	}
+	
+	public boolean isAdmin(Player player) {
+		if (player.hasPermission("animalprotect.admin") || player.hasPermission("craftoplugin.admin"))
+		{ return true; }
+		return false;
 	}
 }
