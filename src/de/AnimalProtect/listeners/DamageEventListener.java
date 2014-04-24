4f7c2dc2d15b9641065f5a81114cddd6ac5ff995
Fear837 <fear837@web.de>
2014-04-25 00:03:50 +0200

    +DamageEventListener: Debug-Nachricht hinzugefügt.

--- src/de/AnimalProtect/listeners/DamageEventListener.java
+++ src/de/AnimalProtect/listeners/DamageEventListener.java
@@ -82,6 +82,7 @@ public class DamageEventListener implements Listener {
 		case PRIMED_TNT:
 			TNTPrimed tnt = (TNTPrimed)damager;
 			damager = (Entity) tnt.getSource();
+			Messenger.broadcast("DAMAGER=PRIMED_TNT! source="+tnt.getSource().getType().toString());
 			break;
 		case FISHING_HOOK:
 			Fish fishingHook = (Fish)damager;
