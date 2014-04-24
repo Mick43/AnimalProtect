7bce2bc5bcbceb59b16ed972cfd833cb4a7afe5e
Fear837 <fear837@web.de>
2014-04-24 23:30:03 +0200

    *Command_lock: runCommand verbessert

--- src/de/AnimalProtect/commands/Command_lock.java
+++ src/de/AnimalProtect/commands/Command_lock.java
@@ -1,5 +1,8 @@
 package de.AnimalProtect.commands;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.bukkit.Effect;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
@@ -17,6 +20,7 @@ import de.AnimalProtect.structs.Animal;
 public class Command_lock implements CommandExecutor {
 	
 	private static AnimalProtect plugin;
+	private static HashMap<UUID, Long> lockTimes;
 	
 	public Command_lock(AnimalProtect plugin) {
 		Command_lock.plugin = plugin;
@@ -39,6 +43,8 @@ public class Command_lock implements CommandExecutor {
 			Messenger.sendMessage(cs, "SENDER_NOT_PLAYER");
 			return;
 		}
+		
+		if (lockTimes == null) { lockTimes = new HashMap<UUID, Long>(); }
 		
 		/* Variablen bereitstellen */
 		Player sender = (Player)cs;
@@ -65,18 +71,43 @@ public class Command_lock implements CommandExecutor {
 		else {
 			try {
 				if (plugin.getDatenbank().getAnimals(player.getUniqueId()).size() <= plugin.getConfig().getInt("settings.max_entities_for_player")) {
-					animal = new Animal(AnimalProtect.plugin, player, entity);
-					if(animal.saveToDatabase(true)) { 
-						Messenger.sendMessage(cs, "LOCK_SUCCESS"); 
-						entity.getWorld().playEffect(entity.getLocation(), Effect.SMOKE, 100);
-						entity.getWorld().playEffect(entity.getLocation(), Effect.SMOKE, 100);
-						entity.getWorld().playEffect(entity.getLocation(), Effect.SMOKE, 100);
-						entity.getWorld().playSound(entity.getLocation(), Sound.CLICK, 0.5f, 1.75f);
-						entity.getWorld().playSound(entity.getLocation(), Sound.CLICK, 0.5f, 1.75f);
-						entity.getWorld().playSound(entity.getLocation(), Sound.CLICK, 0.5f, 1.75f);
-						sender.playSound(sender.getLocation(), Sound.CLICK, 0.75f, 1.75f);
+					if (lockTimes.containsKey(sender.getUniqueId())) {
+						if (lockTimes.get(sender.getUniqueId()) + 5000 < System.currentTimeMillis()) {
+							animal = new Animal(AnimalProtect.plugin, player, entity);
+							if(animal.saveToDatabase(true)) { 
+								Messenger.sendMessage(cs, "LOCK_SUCCESS"); 
+								entity.getWorld().playEffect(entity.getLocation(), Effect.SMOKE, 100);
+								entity.getWorld().playEffect(entity.getLocation(), Effect.SMOKE, 100);
+								entity.getWorld().playEffect(entity.getLocation(), Effect.SMOKE, 100);
+								entity.getWorld().playSound(entity.getLocation(), Sound.CLICK, 0.5f, 1.75f);
+								entity.getWorld().playSound(entity.getLocation(), Sound.CLICK, 0.5f, 1.75f);
+								entity.getWorld().playSound(entity.getLocation(), Sound.CLICK, 0.5f, 1.75f);
+								sender.playSound(sender.getLocation(), Sound.CLICK, 0.75f, 1.75f);
+								lockTimes.put(sender.getUniqueId(), System.currentTimeMillis());
+							}
+							else { Messenger.sendMessage(cs, "LOCK_FAILED"); }
+						}
+						else {
+							Integer wait = (int) (lockTimes.get(sender.getUniqueId()) + 5000 - System.currentTimeMillis());
+							wait = wait / 1000;
+							Messenger.sendMessageFailed(cs, "Fehler: Warte noch "+wait+" Sekunden, bevor du das nächste Tier protectest.");
+						}
 					}
-					else { Messenger.sendMessage(cs, "LOCK_FAILED"); }
+					else {
+						animal = new Animal(AnimalProtect.plugin, player, entity);
+						if(animal.saveToDatabase(true)) { 
+							Messenger.sendMessage(cs, "LOCK_SUCCESS"); 
+							entity.getWorld().playEffect(entity.getLocation(), Effect.SMOKE, 100);
+							entity.getWorld().playEffect(entity.getLocation(), Effect.SMOKE, 100);
+							entity.getWorld().playEffect(entity.getLocation(), Effect.SMOKE, 100);
+							entity.getWorld().playSound(entity.getLocation(), Sound.CLICK, 0.5f, 1.75f);
+							entity.getWorld().playSound(entity.getLocation(), Sound.CLICK, 0.5f, 1.75f);
+							entity.getWorld().playSound(entity.getLocation(), Sound.CLICK, 0.5f, 1.75f);
+							sender.playSound(sender.getLocation(), Sound.CLICK, 0.75f, 1.75f);
+							lockTimes.put(sender.getUniqueId(), System.currentTimeMillis());
+						}
+						else { Messenger.sendMessage(cs, "LOCK_FAILED"); }
+					}
 				}
 				else { Messenger.sendMessage(cs, "MAX_LOCKS_EXCEEDED"); }
 			}
