f14d513c8bd3dc1fa6fd711c7766a0a85edd8357
Fear837 <fear837@web.de>
2014-04-25 00:19:28 +0200

    *InteractEventListener: mit try/catch umgeben

--- src/de/AnimalProtect/listeners/InteractEventListener.java
+++ src/de/AnimalProtect/listeners/InteractEventListener.java
@@ -37,97 +37,100 @@ public class InteractEventListener implements Listener {
 	
 	@EventHandler
 	public void onEntityEvent(PlayerInteractEntityEvent event) {
-		if (!plugin.isEnabled() || event.isCancelled()) { return; }
-		
-		/* Prüfen ob die HashMaps null sind */
-		if (InteractEventListener.selectedList == null) { InteractEventListener.selectedList = new HashMap<UUID, Entity>(); }
-		if (InteractEventListener.selectedTime == null) { InteractEventListener.selectedTime = new HashMap<UUID, Long>(); }
-		
-		/* Datenbank-Verbindung aufbauen, falls nicht vorhanden. */
-		if (!database.isConnected()) { database.connect(); }
-		
-		if (plugin.isEnabled() && database.isConnected() && !event.isCancelled()) {
-			/* Wenn der Spieler nicht geduckt ist, dann wird returned. */
-			if (!event.getPlayer().isSneaking()) { return; }
-			/* Wenn das Entity kein Tier ist, dann wird returned. */
-			if (!plugin.isAnimal(event.getRightClicked())) { return; }
+		try {
+			if (!plugin.isEnabled() || event.isCancelled()) { return; }
 			
-			/* Die Variablen werden initialisiert. */
-			Player player = event.getPlayer();
-			Entity entity = event.getRightClicked();
-			CraftoPlayer owner = null;
+			/* Prüfen ob die HashMaps null sind */
+			if (InteractEventListener.selectedList == null) { InteractEventListener.selectedList = new HashMap<UUID, Entity>(); }
+			if (InteractEventListener.selectedTime == null) { InteractEventListener.selectedTime = new HashMap<UUID, Long>(); }
 			
-			/* Prüfen ob das ausgewählte Tier bereits vom Spieler ausgewählt ist. */
-			if (InteractEventListener.selectedList.containsKey(player.getUniqueId()) && InteractEventListener.selectedList.get(player.getUniqueId()).equals(entity)) {
-				Messenger.sendMessage(player, "SELECTED_ALREADY");
-				player.playSound(player.getLocation(), Sound.CLICK, 0.4f, 0.8f);
-				return;
-			}
+			/* Datenbank-Verbindung aufbauen, falls nicht vorhanden. */
+			if (!database.isConnected()) { database.connect(); }
 			
-			/* Den Owner des Entities festlegen, null falls Entity nicht locked. */
-			owner = database.getOwner(entity.getUniqueId());
-			
-			/* Wenn das Tier nicht gelockt ist, dann wird der Owner nicht erwähnt. */
-			if (owner == null) {
-				switch (entity.getType()) {
-				case COW:
-					Messenger.sendMessage(player, "SELECTED_COW");
-					break;
-				case PIG:
-					Messenger.sendMessage(player, "SELECTED_PIG");
-					break;
-				case CHICKEN:
-					Messenger.sendMessage(player, "SELECTED_CHICKEN");
-					break;
-				case HORSE:
-					Messenger.sendMessage(player, "SELECTED_HORSE");
-					break;
-				case WOLF:
-					Messenger.sendMessage(player, "SELECTED_WOLF");
-					break;
-				case SHEEP:
-					Messenger.sendMessage(player, "SELECTED_SHEEP");
-					break;
-				case IRON_GOLEM:
-					Messenger.sendMessage(player, "SELECTED_IRONGOLEM");
-					break;
-				case SNOWMAN:
-					Messenger.sendMessage(player, "SELECTED_SNOWMAN");
-					break;
-				case VILLAGER:
-					Messenger.sendMessage(player, "SELECTED_VILLAGER");
-					break;
-				case OCELOT:
-					Messenger.sendMessage(player, "SELECTED_OCELOT");
-					break;
-				default:
-					Messenger.sendMessage(player, "SELECTED_UNKNOWN");
-					break;
-				}
-			}
-			/* Wenn das Tier protected ist, dann wird der Owner erwähnt. */
-			else {
-				Messenger.sendMessage(player, "Du hast das Tier von §6"+owner.getName()+" §eausgewählt.");
+			if (plugin.isEnabled() && database.isConnected() && !event.isCancelled()) {
+				/* Wenn der Spieler nicht geduckt ist, dann wird returned. */
+				if (!event.getPlayer().isSneaking()) { return; }
+				/* Wenn das Entity kein Tier ist, dann wird returned. */
+				if (!plugin.isAnimal(event.getRightClicked())) { return; }
 				
-				try {
-					/* Wenn seit dem letzten Select 30 Sekunden vergangen sind */
-					if (InteractEventListener.selectedTime.containsKey(player.getUniqueId())) {
-						if (InteractEventListener.selectedTime.get(player.getUniqueId()) + 60000 < System.currentTimeMillis()) {
-							Animal animal = database.getAnimal(entity.getUniqueId());
-							if (animal != null) { 
-								animal.updateAnimal(entity);
-								animal.saveToDatabase(true);
+				/* Die Variablen werden initialisiert. */
+				Player player = event.getPlayer();
+				Entity entity = event.getRightClicked();
+				CraftoPlayer owner = null;
+				
+				/* Prüfen ob das ausgewählte Tier bereits vom Spieler ausgewählt ist. */
+				if (InteractEventListener.selectedList.containsKey(player.getUniqueId()) && InteractEventListener.selectedList.get(player.getUniqueId()).equals(entity)) {
+					Messenger.sendMessage(player, "SELECTED_ALREADY");
+					player.playSound(player.getLocation(), Sound.CLICK, 0.4f, 0.8f);
+					return;
+				}
+				
+				/* Den Owner des Entities festlegen, null falls Entity nicht locked. */
+				owner = database.getOwner(entity.getUniqueId());
+				
+				/* Wenn das Tier nicht gelockt ist, dann wird der Owner nicht erwähnt. */
+				if (owner == null) {
+					switch (entity.getType()) {
+					case COW:
+						Messenger.sendMessage(player, "SELECTED_COW");
+						break;
+					case PIG:
+						Messenger.sendMessage(player, "SELECTED_PIG");
+						break;
+					case CHICKEN:
+						Messenger.sendMessage(player, "SELECTED_CHICKEN");
+						break;
+					case HORSE:
+						Messenger.sendMessage(player, "SELECTED_HORSE");
+						break;
+					case WOLF:
+						Messenger.sendMessage(player, "SELECTED_WOLF");
+						break;
+					case SHEEP:
+						Messenger.sendMessage(player, "SELECTED_SHEEP");
+						break;
+					case IRON_GOLEM:
+						Messenger.sendMessage(player, "SELECTED_IRONGOLEM");
+						break;
+					case SNOWMAN:
+						Messenger.sendMessage(player, "SELECTED_SNOWMAN");
+						break;
+					case VILLAGER:
+						Messenger.sendMessage(player, "SELECTED_VILLAGER");
+						break;
+					case OCELOT:
+						Messenger.sendMessage(player, "SELECTED_OCELOT");
+						break;
+					default:
+						Messenger.sendMessage(player, "SELECTED_UNKNOWN");
+						break;
+					}
+				}
+				/* Wenn das Tier protected ist, dann wird der Owner erwähnt. */
+				else {
+					Messenger.sendMessage(player, "Du hast das Tier von §6"+owner.getName()+" §eausgewählt.");
+					
+					try {
+						/* Wenn seit dem letzten Select 30 Sekunden vergangen sind */
+						if (InteractEventListener.selectedTime.containsKey(player.getUniqueId())) {
+							if (InteractEventListener.selectedTime.get(player.getUniqueId()) + 60000 < System.currentTimeMillis()) {
+								Animal animal = database.getAnimal(entity.getUniqueId());
+								if (animal != null) { 
+									animal.updateAnimal(entity);
+									animal.saveToDatabase(true);
+								}
 							}
 						}
 					}
+					catch (Exception e) { Messenger.exception("InteractEventListener.onEntityEvent", "Exception :(", e); }
 				}
-				catch (Exception e) { Messenger.exception("InteractEventListener.onEntityEvent", "Exception :(", e); }
+				
+				/* Zum Schluss wird bei dem Spieler noch ein Sound abgespielt und sein zuletzt ausgewähltes Tier wird gespeichert. */
+				player.playSound(player.getLocation(), Sound.CLICK, 0.75f, 0.8f);
+				addSelected(player.getUniqueId(), entity);
 			}
-			
-			/* Zum Schluss wird bei dem Spieler noch ein Sound abgespielt und sein zuletzt ausgewähltes Tier wird gespeichert. */
-			player.playSound(player.getLocation(), Sound.CLICK, 0.75f, 0.8f);
-			addSelected(player.getUniqueId(), entity);
 		}
+		catch (Exception e) { Messenger.exception("InteractEventListener/onEntityEvent", "Unknown Exception", e); }
 	}
 	
 	private void addSelected(UUID uuid, Entity entity) {
